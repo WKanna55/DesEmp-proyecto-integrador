c71d95f1ef06eced6850e445760a09cbcea809e8
Your Name <you@example.com>
2024-06-15 19:15:21 -0500

    tabla12

--- desempProjectFinal/brazalete_datos/firebase.py
+++ desempProjectFinal/brazalete_datos/firebase.py
@@ -4,7 +4,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Ruta correcta al archivo JSON de credenciales
-cred = credentials.Certificate('brazalete_datos\\prueba-android-2d2fc-firebase-adminsdk-p14z7-04148ab8e7.json')
+cred = credentials.Certificate('brazalete_datos\\credenciales.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://prueba-android-2d2fc-default-rtdb.firebaseio.com/'
 })
